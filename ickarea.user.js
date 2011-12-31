745f3cb3ab72ea9d2ca70b38a5e2790688746eca
Zac <mymengyu@gmail.com>
2011-12-31 12:50:37 +0800

    Add ickarea.user.js

---
+++ ickarea.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name    iCKarea
+// @namespace   iCkarea
+// @auther  Zac
+// @description
+// @include http://v.ckarea.com/bbs/forum.php?mod=viewthread*
+// @version 1.1
+// ==/UserScript==
+
+document.getElenentById("pushGood").style.display="none";
+
+document.getElenentById("realContent").style.display="";
