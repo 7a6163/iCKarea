ab99914c3e3687c180b66669686c429095bb776a
Zac <mymengyu@gmail.com>
2012-01-02 19:32:39 +0800

    Modify Description

--- ickarea.user.js
+++ ickarea.user.js
@@ -1,12 +1,11 @@
 // ==UserScript==
-// @name    iCKarea
+// @name        iCKarea
 // @namespace   iCkarea
-// @auther  Zac
-// @description
-// @include http://v.ckarea.com/bbs/forum.php?mod=viewthread*
-// @version 1.1
+// @auther      Zac
+// @description 顯示隱藏的訊息
+// @include     http://v.ckarea.com/bbs/forum.php?mod=viewthread*
+// @version     1.1
 // ==/UserScript==
 
 document.getElenentById("pushGood").style.display="none";
-
 document.getElenentById("realContent").style.display="";
