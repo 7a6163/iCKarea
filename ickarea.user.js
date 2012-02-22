64df956bb9908d3ddda90c5964dcfff0c77efec0
Zac <mymengyu@gmail.com>
2012-02-22 10:51:06 +0800

    Fixed Image can't see

--- ickarea.user.js
+++ ickarea.user.js
@@ -3,9 +3,24 @@
 // @namespace   iCkarea
 // @auther      Zac
 // @description 顯示隱藏的訊息
-// @include     http://v.ckarea.com/bbs/forum.php?mod=viewthread*
-// @version     1.2
+// @include     http://tellfriendsfast.com/bbs/forum.php*
+// @include     http://v.ckarea.com/bbs/forum.php*
+// @version     1.5
 // ==/UserScript==
 
-document.getElementById("pushGood").style.display="none";
-document.getElementById("realContent").style.display="";
+
+
+var img = document.getElementsByTagName("img");
+var regex =/www\.ckarea/;
+
+for (var i = 0; i < img.length; i++)
+{
+    if(regex.test(img[i].src))
+        img[i].src = img[i].src.replace(regex,"v.ckarea");
+}
+
+if(document.getElementById("pushGood") != null){
+    document.getElementById("pushGood").style.display="none";
+    document.getElementById("realContent").style.display="";
+}
+
